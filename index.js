71381303b19aaa143daf8f30c90f54ac0a5d0f15
kashafsaeed7842@gmail.com <kashafsaeed7842@gmail.com.noreply.github.com>
2025-09-19 18:16:24 +0500

    commit

---
+++ index.js
@@ -0,0 +1,16 @@
+
+    var ramzanDate = moment("2026-02-28 00:00:00"); 
+
+  function updateCountdown() {
+    var now = moment();
+    var diff = moment.duration(ramzanDate.diff(now));
+    document.getElementById("Months").textContent  = Math.floor(diff.asMonths());
+    document.getElementById("weeks").textContent   = Math.floor(diff.asWeeks()) % 4;
+    document.getElementById("days").textContent    = diff.days();
+    document.getElementById("hours").textContent   = diff.hours();
+    document.getElementById("minutes").textContent = diff.minutes();
+    document.getElementById("seconds").textContent = diff.seconds();
+  }
+
+  setInterval(updateCountdown, 1000);
+  updateCountdown();
